80524b986e539ccd8cef955aecb3b0f5f3f80239
DESKTOP-Q5ASHFU\weber <danaipengpeng@163.com>
2017-07-31 17:59:46 +0800

    思考

--- src/index.js
+++ src/index.js
@@ -15,6 +15,8 @@ import StateCalculator from './StateCalculator';
 import Calculator from './StateCalculator1.js';
 import WelcomeDialog from './ContainerChildren.js';
 import CustomChildren from './CustomChildren';
+import Product from './Product';
+
 
 
 import registerServiceWorker from './registerServiceWorker';
@@ -112,8 +114,24 @@ import registerServiceWorker from './registerServiceWorker';
 // 	document.getElementById('root')
 // )
 
+// ReactDOM.render(
+// 	<CustomChildren />,
+// 	document.getElementById('root')
+// )
+
+var PRODUCTS = [
+  {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
+  
+  
+  {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
+  {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
+  {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
+  {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
+  {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
+]
+
 ReactDOM.render(
-	<CustomChildren />,
+	<Product products={PRODUCTS} />,
 	document.getElementById('root')
 )
 
